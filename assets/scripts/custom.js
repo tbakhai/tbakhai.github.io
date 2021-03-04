4a1f80476abab5c191d57794e8f5834046b6b2c9
Ba Thien Tran <bathientran@gmail.com>
2021-03-03 21:42:18 -0700

    Adds better loading

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -7,9 +7,9 @@ $(function () {
 
 function switchSection() {
     var x = document.getElementsByClassName('bad-section')[0];
-    x.style.display = x.style.display === 'none' ? '' : 'none';
+    x.style.visibility = x.style.visibility === 'hidden' ? 'visible' : 'hidden';
     var y = document.getElementsByClassName('good-section')[0];
-    y.style.display = y.style.display === 'none' ? '' : 'none';
+    y.style.visibility = y.style.visibility === 'hidden' ? 'visible' : 'hidden';
     var z = document.getElementsByClassName('landing-section-minor')[0];
     console.log(z.style["background-color"])
     z.style["background-color"] = z.style["background-color"] === 'rgba(21, 0, 255, 0.027)' ? '#E75B1007' : 'rgba(21, 0, 255, 0.027)';
